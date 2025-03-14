cf5bc0fbde00407ebc19f548005bf67fbf48d7bc
Zubair Syed <zubair.syed@ten10.com>
2025-03-14 22:55:33 +0000

    Added response.timings object to print response time seperately in console output.

--- advancedtest.js
+++ advancedtest.js
@@ -26,12 +26,19 @@ if (__ENV.scenario) {
 export function getCrocodilesAndCrocodileByIdTest(){
   let res1 = http.get(`https://test-api.k6.io/public/crocodiles/`);
   check(res1, { "status is 200": (res1) => res1.status === 200 });
-  console.log("This is the json response",res1.json());                                           
+  
+  console.log('Response time of getCrocodiles was ' + String(res1.timings.duration) + ' ms');
+  console.log("This is the json response",res1.json()); 
+
   const crocIds = Object.values(res1.json()).map(j => j.id)      //reading json response and getting specifically id values to be stored into variable. 
   console.log("These are all the ids",crocIds)
+  
   const randomId = crocIds[Math.floor(Math.random() * crocIds.length)];
   console.log("The randomly chosen id is:",randomId);
+  
   let res2 = http.get(`https://test-api.k6.io/public/crocodiles/${randomId.toString()}`);
   check(res2, { "status is 200": (res2) => res2.status === 200 });
+  
+  console.log('Response time of getCrocodileById was ' + String(res2.timings.duration) + ' ms');
   console.log(res2.json());
 }
