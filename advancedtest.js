8a835a942985375d13b29c4fbb054e3469eba0a9
Zubair Syed <zubair.syed@ten10.com>
2025-03-14 18:27:38 +0000

    Merged two seperate test functions into one test function, for ease of scenario making.
    As I want to now start doing multiple scenarios for load testing, I think having two seperate test functions is a bit out of scope and too convoluted at this timescale. Hence I have merged the two functions into one, 'getCrocodilesAndCrocodileByIdTest()'

--- advancedtest.js
+++ advancedtest.js
@@ -5,22 +5,13 @@ let scenarios = {
   first_test: {
     executor: 'shared-iterations',
     vus: 1,
-    exec: 'getCrocodilesTest',
-    iterations: 1,
-  },
-  second_test: {
-    executor: 'shared-iterations',
-    vus: 1,
-    tags: { test_type: 'api' }, 
-    exec: 'getCrocodileByIdTest', 
+    exec: 'getCrocodilesAndCrocodileByIdTest',
     iterations: 1,
   },
 };
 
 export let options = {
   scenarios : {},
-  thresholds: {
-  },
 };
 
 
@@ -32,21 +23,15 @@ if (__ENV.scenario) {
 }
 
    
-export function getCrocodilesTest(){
-  let res = http.get(`https://test-api.k6.io/public/crocodiles/`);
-  check(res, { "status is 200": (res) => res.status === 200 });
-  console.log("This is the json response",res.json()); 
-  const jsonRes = res.json();                                          
-  const crocIds = Object.values(jsonRes).map(j => j.id)      //reading json response and getting specifically id values to be stored into variable. 
+export function getCrocodilesAndCrocodileByIdTest(){
+  let res1 = http.get(`https://test-api.k6.io/public/crocodiles/`);
+  check(res1, { "status is 200": (res1) => res1.status === 200 });
+  console.log("This is the json response",res1.json());                                           
+  const crocIds = Object.values(res1.json()).map(j => j.id)      //reading json response and getting specifically id values to be stored into variable. 
   console.log("These are all the ids",crocIds)
-}
-
-export function getCrocodileByIdTest() {
-  const jsonRes = (http.get(`https://test-api.k6.io/public/crocodiles/`)).json()
-  const crocIds = Object.values(jsonRes).map(j => j.id)
   const randomId = crocIds[Math.floor(Math.random() * crocIds.length)];
   console.log("The randomly chosen id is:",randomId);
-  let res = http.get(`https://test-api.k6.io/public/crocodiles/${randomId.toString()}`);
-  check(res, { "status is 200": (res) => res.status === 200 });
-  console.log(res.json());
+  let res2 = http.get(`https://test-api.k6.io/public/crocodiles/${randomId.toString()}`);
+  check(res2, { "status is 200": (res2) => res2.status === 200 });
+  console.log(res2.json());
 }
